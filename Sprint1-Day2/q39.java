094e36727df998511c4c6ba3d6467b5ea8d608e8
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-05-13 22:31:28 +0530

    Create q39.java

---
+++ Sprint1-Day2/q39.java
@@ -0,0 +1,7 @@
+public class q39{
+    public static void main(String[] args) {
+        Integer myInteger = 150;
+        Float convertedFloat = myInteger.floatValue();
+        System.out.println("Converted Float: " + convertedFloat);
+    }
+}
