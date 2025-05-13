0153a8eeaf29c630228da2dcd8da6d0f1a3e4963
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-05-13 22:29:37 +0530

    Create q37.java

---
+++ Sprint1-Day2/q37.java
@@ -0,0 +1,8 @@
+public class q37 {
+    public static void main(String[] args) {
+        String textA = "Good ";
+        String textB = "Morning!";
+        String combinedText = textA + textB;  
+        System.out.println("Final message: " + combinedText);
+    }
+}
