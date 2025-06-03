c5877fa38f2b5b7ff1eed88734364d398c53e322
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-03 23:18:43 +0530

    Create utils.js

---
+++ Sprint2Day6/utils.js
@@ -0,0 +1,16 @@
+
+export const log = (...args) => console.log(...args);
+
+export const renderList = (todos = []) => {
+  return todos.map(todo => todo.toHTML()).join("");
+};
+
+export const mergeObjects = (obj1, obj2 = {}) => {
+  return Object.assign({}, obj1, obj2);
+};
+
+export const compare = (a, b) => Object.is(a, b);
+
+export const createTodosFromTitles = (...titles) => {
+  return titles.map(title => ({ title, done: false }));
+};
