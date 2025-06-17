2bbbd0df395582b7fb42149cd8ed5a91856688eb
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-17 12:17:02 +0530

    Create App.js

---
+++ Sprint3Day5/q7/App.js
@@ -0,0 +1,16 @@
+import UserProfileNew from './UserProfileNew';
+import NotificationsNew from './NotificationsNew';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Alice" />
+      <CounterNew />
+      <ProductCardNew />
+      <ThemeToggleNew />
+      <TodoListNew />
+      <UserProfileNew />
+      <NotificationsNew />
+    </div>
+  );
+}
