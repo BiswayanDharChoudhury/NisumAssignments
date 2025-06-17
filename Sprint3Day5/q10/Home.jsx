91be37b2158fda8e897a422b3ad65c1991cfe6db
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-17 22:20:38 +0530

    Create Home.jsx

---
+++ Sprint3Day5/q10/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main content area of the home page.</p>
+    </Layout>
+  );
+};
+
+export default Home;
