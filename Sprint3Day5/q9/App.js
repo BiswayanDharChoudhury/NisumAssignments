e0a156ea8ea291f95ed7c42cda71d122b40e40d7
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-17 17:51:12 +0530

    Create App.js

---
+++ Sprint3Day5/q9/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main style={{ padding: "20px" }}>
+        <h1>Welcome to MySite!</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
