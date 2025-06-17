81bc0220d0ca1e77b2a8fa0c1cb596ba46c95e88
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-17 22:20:56 +0530

    Create About.jsx

---
+++ Sprint3Day5/q10/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>We are building reusable components with React and CSS Grid.</p>
+    </Layout>
+  );
+};
+
+export default About;
