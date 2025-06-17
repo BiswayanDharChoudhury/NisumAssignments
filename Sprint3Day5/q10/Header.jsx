d3497d516f71f28161690b7a5073b8d8069e6700
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-17 22:19:01 +0530

    Create Header.jsx

---
+++ Sprint3Day5/q10/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h2>MySite Header</h2>
+    </header>
+  );
+};
+
+export default Header;
