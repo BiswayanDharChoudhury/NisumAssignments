f6c9fdcbb1aecfe2713f08f2066f8faf7999a92c
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-30 12:38:00 +0530

    Create CentralizedExceptionApp.java

---
+++ Sprint3Day3/q2/CentralizedExceptionApp.java
@@ -0,0 +1,11 @@
+package com.nisum.NisumAssignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedExceptionApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CentralizedExceptionApp.class, args);
+    }
+}
