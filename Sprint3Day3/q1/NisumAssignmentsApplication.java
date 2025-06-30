06ce2e830796d1be2ba3c759330ffa48495dc963
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-30 12:30:56 +0530

    Create NisumAssignmentsApplication.java

---
+++ Sprint3Day3/q1/NisumAssignmentsApplication.java
@@ -0,0 +1,11 @@
+package com.nisum.NisumAssignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumAssignmentsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NisumAssignmentsApplication.class, args);
+	}
+}
