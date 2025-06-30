af9eb80892f626f7776e45ab3b8859542b3cab89
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-06-30 13:14:09 +0530

    Create EcomResourceNotFoundException.java

---
+++ Sprint3Day3/q5/EcomResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.nisum.NisumAssignments;
+
+public class EcomResourceNotFoundException extends RuntimeException {
+    public EcomResourceNotFoundException(String message) {
+        super(message);
+    }
+}
