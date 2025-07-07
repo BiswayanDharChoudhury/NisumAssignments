ca480bc4b42d47af720653410d389a2f4badd30c
Biswayan_DC <137798229+BiswayanDharChoudhury@users.noreply.github.com>
2025-07-07 12:57:18 +0530

    Create Products.js

---
+++ Sprint3Day6/q5/src/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Products() {
+  return (
+    <div>
+      <h2>Product List</h2>
+      {}
+    </div>
+  );
+}
+
+export default Products;
